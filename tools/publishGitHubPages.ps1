3c7af6a00deb9db992724b7c46e72b7659fb1542
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-05-30 13:28:43 -0400

    Attempt to fix publishGitHubPages; no deploy for now

--- tools/publishGitHubPages.ps1
+++ tools/publishGitHubPages.ps1
@@ -1,16 +1,35 @@
 # pushes build/html to gh-pages branch
 $msg = 'publishGitHubPages: build/html -> gh-pages'
+$gitURL = "https://github.com/intellifactory/websharper.d3.git"
+$inAV = false
+if ($env:APPVEYOR_BUILD_NUMBER) { $inAV = true; }
+
 write-host -foregroundColor "green" "=====> $msg"
-rm -r build/gh-pages -errorAction ignore
-$d = mkdir -force build
-cd build
-git clone .. gh-pages
-cd gh-pages
+
+function clearDir() {
+  rm -r build/gh-pages -errorAction ignore
+}
+
+if ($inAV) {
+  git config credential.helper "store --file=.git/credentials"
+  echo ("https://$GH_TOKEN" + ":@github.com") > .git/credentials	
+  git config user.name "AppVeyor"
+  git config user.email "websharper-support@intellifactory.com"
+  clearDir
+  git clone $gitURL build/gh-pages  
+  cd build/gh-pages
+} else {
+  clearDir
+  cd build
+  git clone .. gh-pages
+  cd gh-pages
+}
+
 git checkout gh-pages
-cp -r -force ../../build/html/* .
+cp -r -force ../html/* .
 git add .
 git commit -am $msg
 git push origin gh-pages
 cd ../..
-rm -r build/gh-pages -errorAction ignore
+clearDir
 write-host -foregroundColor "green" "=====> DONE"
