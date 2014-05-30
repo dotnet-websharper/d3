6ecfe520e312e6724b78656179b20985b8f704d9
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-05-30 14:03:39 -0400

    Attempt to redirect stderr.

--- tools/publishGitHubPages.ps1
+++ tools/publishGitHubPages.ps1
@@ -6,30 +6,31 @@ $gitURL = "https://github.com/intellifactory/websharper.d3.git"
 
 write-host -foregroundColor "green" "=====> $msg"
 
+
 function clearDir() {
   rm -r build/gh-pages -errorAction ignore
 }
 
 if ($env -eq "appveyor") {
+  clearDir
+  git clone $gitURL build/gh-pages 2>git.log
+  cd build/gh-pages
   git config credential.helper "store --file=.git/credentials"
   echo ("https://$GH_TOKEN" + ":@github.com") > .git/credentials	
   git config user.name "AppVeyor"
   git config user.email "websharper-support@intellifactory.com"
-  clearDir
-  git clone $gitURL build/gh-pages  
-  cd build/gh-pages
 } else {
   clearDir
   cd build
-  git clone .. gh-pages
+  git clone .. gh-pages 2>git.log
   cd gh-pages
 }
 
-git checkout gh-pages
+git checkout gh-pages 2>git.log
 cp -r -force ../html/* .
-git add .
-git commit -am $msg
-git push origin gh-pages
+git add . 2>git.log
+git commit -am $msg 2>git.log
+git push origin gh-pages 2>git.log
 cd ../..
 clearDir
 write-host -foregroundColor "green" "=====> DONE"
