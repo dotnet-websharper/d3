797cc1472637f18aadea4d2e250f31509f533b36
unknown <adam.granicz@IFBUD13.budapest.intellifactory.com>
2014-11-27 19:54:23 +0100

    Added support for building with if.build.

---
+++ build.fsx
@@ -0,0 +1,27 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("WebSharper.D3", "2.5")
+        .References(fun r -> [r.Assembly "System.Web"])
+    |> fun bt -> bt.WithFramework(bt.Framework.Net40)
+
+let main =
+    bt.WebSharper.Extension("IntelliFactory.WebSharper.D3")
+        .SourcesFromProject()
+
+bt.Solution [
+    main
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "WebSharper.D3-d3v3"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://github.com/intellifactory/websharper.d3"
+                Description = "WebSharper Extensions for Google Maps d3v3"
+                RequiresLicenseAcceptance = true })
+        .Add(main)
+
+]
+|> bt.Dispatch
