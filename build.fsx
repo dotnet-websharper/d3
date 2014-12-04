f293c93fe363902037c83fbcd3da6e3d6268c808
Loïc Denuzière <loic@denuziere.net>
2014-12-04 16:37:54 +0100

    Update build.fsx to 3.0-alpha

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.D3", "3.0")
+    BuildTool().PackageId("WebSharper.D3", "3.0-alpha")
         .References(fun r -> [r.Assembly "System.Web"])
     |> fun bt -> bt.WithFramework(bt.Framework.Net40)
 
